a2b05bfe3249dfecd966e8299393275822c27f63
vikramriyer <vikram.iyer09@gmail.com>
2018-05-29 17:35:39 +0530

    settings: changed hardcoded dir paths

--- ttsdaisy_v4/settings.py
+++ ttsdaisy_v4/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = "/data/django_u/django_out"
+STATIC_ROOT = "/django_out/"
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "thanks"
